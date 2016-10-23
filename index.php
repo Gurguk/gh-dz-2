b884aa10aa23f32c50ce5046d68d18ad7da7a0fa
gurguk <you@example.com>
2016-10-23 23:21:51 +0300

    Dev: final changes

---
+++ index.php
@@ -0,0 +1,5 @@
+<h1>Choose one of our services</h1>
+<ul>
+    <li>Make "slug" from string: <a href="slug.php">Slug</a></li>
+    <li>Find out the day of born: <a href="carbon.php">Carbon</a></li>
+</ul>
