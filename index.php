af0242d5e1423f1d1cd9174d3d1fbae4542fac40
gurguk <you@example.com>
2016-10-23 20:53:29 +0300

    Slug library done

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
 use Cocur\Slugify\Slugify;
 
 $slugify = new Slugify();
-echo $slugify->slugify('Hello World!');
+
+?>
+<form action="index.php" method="post">
+    <input type="text" name="text_for_slug" placeholder="write text to convert"/>
+    <input type="submit" />
+</form>
+<textarea><?php echo $slugify->slugify($_POST['text_for_slug']);?></textarea>
